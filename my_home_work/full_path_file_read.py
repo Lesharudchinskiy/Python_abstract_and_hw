3f3730cfc4187252ebb4d7b4c9da0ca7c73908c6
Lesharudchinskiy <48019816+Lesharudchinskiy@users.noreply.github.com>
2021-07-22 16:48:46 +0300

    hws added

---
+++ my_home_work/full_path_file_read.py
@@ -0,0 +1,4 @@
+file_path = 'C:/Users/RudavinAS/PycharmProjects/other_files/pi2.txt'
+with open(file_path) as file_object:
+    content = file_object.read()
+    print(content)
---
+++ my_home_work/read_file_from_path.py
@@ -0,0 +1,4 @@
+file_path = 'text_files/pi.txt'
+with open(file_path) as file_object:
+    content = file_object.read()
+    print(content)
