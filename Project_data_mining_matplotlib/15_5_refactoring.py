3ab1c4fdeac487e6da95dc340744c17c1d5d7bb2
Lesharudchinskiy <48019816+Lesharudchinskiy@users.noreply.github.com>
2021-08-03 16:31:56 +0300

    Add files via upload

---
+++ Project_data_mining_matplotlib/15_5_refactoring.py
@@ -0,0 +1,29 @@
+from random import choice
+
+
+class RandomWalk():
+    """Инициируем начальные координаты и кол-во точек"""
+    def __init__(self, num_points=5000):
+        self.x_value = [0]
+        self.y_value = [0]
+        self.num_points = num_points
+
+    def get_step(self):
+        direction = choice([-1, 1])
+        distance = choice([0, 1, 2, 3, 4])
+        step = direction * distance
+        return step
+
+    def fill_walk(self):
+        while len(self.x_value) < self.num_points:
+            x_step = self.get_step()
+            y_step = self.get_step()
+            # Если шаги равны нулю, то пропускаем итерацию
+            if x_step == 0 and y_step == 0:
+                continue
+            # Делаем шаг от последней координаты
+            next_x = self.x_value[-1] + x_step
+            next_y = self.y_value[-1] + y_step
+            # Добавляем полученное значение
+            self.x_value.append(next_x)
+            self.y_value.append(next_y)
